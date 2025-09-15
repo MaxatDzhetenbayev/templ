b72666cb909cc06b6df2330b0c5645ea429e9ef4
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-09-15 18:11:26 +0500

    fix: обновление параметров функции LocaleLayout для обработки локали как промиса

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -31,12 +31,9 @@ export default async function LocaleLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }) {
-  const { locale } = params;
-  if (!hasLocale(routing.locales, locale)) {
-    notFound();
-  }
+  const { locale } = await params;
 
   const messages = await getMessages();
 
